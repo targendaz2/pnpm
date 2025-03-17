5506ded3650f83c4cc85fea367a3fd85f8d6073e
David G Rosenberg <targendaz2@me.com>
2025-03-16 20:35:33 -0400

    Add PNPM_HOME environment variable and update PATH for pnpm

--- pnpm.plugin.zsh
+++ pnpm.plugin.zsh
@@ -1,2 +1,9 @@
 # enable autocompletion
 eval "$(pnpm completion zsh)"
+
+# set pnpm home
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
